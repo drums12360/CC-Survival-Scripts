6458d0eef924b2c2f3be71c5652a2f50d79f1d70
Julian B <Keigun-Spark@outlook.com>
2022-01-02 19:46:16 +0100

    Added refuel.lua

--- downloader/downloader.lua
+++ downloader/downloader.lua
@@ -6,6 +6,7 @@ local urls = {
 	"https://raw.githubusercontent.com/Keigun-Spark/CC-Survival-Scripts/main/apis/digAPI.lua",
 	"https://raw.githubusercontent.com/Keigun-Spark/CC-Survival-Scripts/main/stripTunnel.lua",
 	"https://raw.githubusercontent.com/Keigun-Spark/CC-Survival-Scripts/main/mineTunnel.lua",
+	"https://raw.githubusercontent.com/Keigun-Spark/CC-Survival-Scripts/standalone/refuel.lua",
 	"https://raw.githubusercontent.com/Keigun-Spark/CC-Survival-Scripts/main/downloader/downloader.lua",
 }
 
