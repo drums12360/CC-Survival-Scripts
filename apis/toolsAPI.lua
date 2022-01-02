ec2745da6819d95369c71c0b950ae3144bd5803b
Julian B <Keigun-Spark@outlook.com>
2022-01-02 21:59:38 +0100

    Moved inv sort to toolsAPI

--- apis/toolsAPI.lua
+++ apis/toolsAPI.lua
@@ -2,28 +2,19 @@ local storage = require("storageAPI")
 
 local tools = {
 	maxSlots = 16,
-	slot = tonumber(turtle.getSelectedSlot()),
-}
-
-local fuelList = {
-	"minecraft:coal",
-	"minecraft:coal_block",
-	"minecraft:charcoal",
-	"mekanism:block_charcoal",
-	"minecraft:lava_bucket",
+	slot = 1,
 }
 
 local junkList = {
-  "minecraft:cobbled_deepslate",
-  "minecraft:tuff",
-  "minecraft:cobblestone",
-  "minecraft:dirt",
-  "minecraft:andesite",
-  "minecraft:diorite",
-  "minecraft:granite",
-  "minecraft:gravel",
+	"minecraft:cobbled_deepslate",
+	"minecraft:tuff",
+	"minecraft:cobblestone",
+	"minecraft:dirt",
+	"minecraft:andesite",
+	"minecraft:diorite",
+	"minecraft:granite",
+	"minecraft:gravel",
 }
-
 
 function tools.findItem(name)
 	if type(name) ~= "string" then
@@ -49,16 +40,52 @@ function tools.findItem(name)
 	return false
 end
 
-function tools.refuel()
-	for i=1, #fuelList do
-		if tools.findItem(fuelList[i]) then
-			turtle.refuel()
-			return true
-		else
-			return false
+function tools.inventorySort()
+	local inv = {}
+	for i=1, tools.maxSlots do
+		inv[i] = turtle.getItemDetail(i)
+	end
+	for i=1, tools.maxSlots do
+		if inv[i] and inv[i].count < 64 then
+		for j=(i+1), tools.maxSlots do
+			if inv[j] and inv[i].name == inv[j].name then
+			if turtle.getItemSpace(i) == 0 then
+				break
+			end
+			turtle.select(j)
+			tools.slot = j
+			local count = turtle.getItemSpace(i)
+			if count > inv[j].count then
+				count = inv[j].count
+			end
+			turtle.transferTo(i, count)
+			inv[i].count = inv[i].count + count
+			inv[j].count = inv[j].count - count
+			if inv[j].count <= 0 then
+				inv[j] = nil
+			end
+			end
+		end
 		end
 	end
+	for i=1, tools.maxSlots do
+		if not inv[i] then
+		for j=(i+1), tools.maxSlots do
+			if inv[j] then
+			turtle.select(j)
+			tools.slot = j
+			turtle.transferTo(i)
+			inv[i] = data.copyTable(inv[j])
+			inv[j] = nil
+			break
+			end
+		end
+		end
+	end
+	turtle.select(1)
+	tools.slot = 1
 end
+
 
 function tools.place(blockName, direction)
 	tools.findItem(blockName)
@@ -99,7 +126,7 @@ function tools.dropJunk()
 		end
 		end
 	end
-	storage.inventorySort()
+	tools.inventorySort()
 end
 
 return tools
