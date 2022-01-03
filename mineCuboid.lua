e353427b2f388e49be054b85d5a0ec414864b86a
Julian B <Keigun-Spark@outlook.com>
2022-01-03 01:08:21 +0100

    Update mineCuboid.lua

--- mineCuboid.lua
+++ mineCuboid.lua
@@ -26,29 +26,23 @@ function mineSquence(width, height, depth, side)
 		term.clear()
 		term.setCursorPos(1,1)
 	end
-	if side == left or nil then
-		move.turnLeft()
-		move.forward(width)
-		move.turnRight()
+	if side == "left" or side == tostring(nil) then
 		move.up()
-	elseif side == right then
-		move.up()
-	end
-	for x=1,depth do
-		move.forward()
-		tools.dig("up")
-		tools.dig("down")
+		for x=1, depth do
+			move.forward()
+			tools.dig("up")
+			tools.dig("down")
 		if x % 3 == 0 and lastRowCount % 2 == 1 then
-		move.turnRight()
+			move.turnLeft()
 		else
 		if lastRowCount % 2 == 0 then
-			move.turnRight()
-		else
 			move.turnLeft()
+		else
+			move.turnRight()
 		end
 		end
-		for z=1,rows do
-			for y=1,width - 1 do
+		for z=1, rows do
+			for y=1, width - 1 do
 				move.forward()
 				tools.dig("up")
 				tools.dig("down")
@@ -69,44 +63,111 @@ function mineSquence(width, height, depth, side)
 					move.down(offset)
 					tools.dig("down")
 					move.turnAround()
-			else
-				move.up(offset)
-				tools.dig("up")
-				move.turnAround()
-			end
-			for y=1,width - 1 do
-				move.forward()
-				if x % 2 == 0 then
-					tools.dig("down")
 				else
+					move.up(offset)
 					tools.dig("up")
+					move.turnAround()
 				end
+				for y=1, width - 1 do
+					move.forward()
+					if x % 2 == 0 then
+						tools.dig("down")
+					else
+						tools.dig("up")
+					end
+				end
+				lastRowCount = z + 1
 			end
-			lastRowCount = z + 1
 		end
-	end
-	if x % 3 == 2 and lastRowCount % 2 == 1 then
-		move.turnRight()
-	else
+		if x % 3 == 2 and lastRowCount % 2 == 1 then
+			move.turnLeft()
+		else
+			if lastRowCount % 2 == 0 then
+				move.turnLeft()
+			else
+				move.turnRight()
+			end
+		end
+		tools.dropJunk()
+		end
+	elseif side == "right" then
+		move.up()
+		for x=1, depth do
+			move.forward()
+			tools.dig("up")
+			tools.dig("down")
+		if x % 3 == 0 and lastRowCount % 2 == 1 then
+			move.turnRight()
+		else
 		if lastRowCount % 2 == 0 then
 			move.turnRight()
 		else
 			move.turnLeft()
 		end
-	end
-	tools.dropJunk()
+		end
+		for z=1, rows do
+			for y=1, width - 1 do
+				move.forward()
+				tools.dig("up")
+				tools.dig("down")
+			end
+			lastRowCount = z
+			if z ~= rows then
+				if x % 2 == 0 then
+					move.down(3)
+					tools.dig("down")
+					tools.turnAround()
+				else
+					move.up(3)
+					tools.dig("up")
+					move.turnAround()
+				end
+			elseif offset ~= 0 then
+				if x % 2 == 0 then
+					move.down(offset)
+					tools.dig("down")
+					move.turnAround()
+				else
+					move.up(offset)
+					tools.dig("up")
+						move.turnAround()
+				end
+				for y=1, width - 1 do
+					move.forward()
+					if x % 2 == 0 then
+						tools.dig("down")
+					else
+						tools.dig("up")
+					end
+				end
+				lastRowCount = z + 1
+			end
+		end
+		if x % 3 == 2 and lastRowCount % 2 == 1 then
+			move.turnRight()
+		else
+			if lastRowCount % 2 == 0 then
+				move.turnRight()
+			else
+				move.turnLeft()
+			end
+		end
+		tools.dropJunk()
+		end
+	elseif side ~= "left" or side ~= "left" or side ~= tostring(nil) then
+		error(side, "is not a valid direction!")
 	end
 end
 
-if type(tonumber(tArgs[1])) and type(tonumber(tArgs[2])) and type(tonumber(tArgs[3])) ~= "number" and type(tostring(tArgs[4])) ~= "string" or type(tostring(tArgs[4])) ~= nil then
+if type(tonumber(tArgs[1])) and type(tonumber(tArgs[2])) and type(tonumber(tArgs[3])) ~= "number" then
 	term.clear()
 	term.setCursorPos(1,1)
-	error("Width, height and depth are required! (Example: '5 5 10 right') [5 blocks wide, 5 block heigh, 10 blocks deep and turtle placed on right of cuboid]")
+	error("Width, height and depth are required! (Example: '5 5 10 right') [5 blocks wide, 5 block heigh, 10 blocks deep and turtle placed on right corner of cuboid]")
 end
 
 local start = data.copyTable(data.coords)
 data.saveData("/.save", "/start_pos", start)
-mineSquence(tonumber(tArgs[1]), tonumber(tArgs[2]), tonumber(tArgs[3]), (tostring(tArgs[3])))
+mineSquence(tonumber(tArgs[1]), tonumber(tArgs[2]), tonumber(tArgs[3]), (tostring(tArgs[4])))
 move.moveTo("~",start.y + 1,"~")
 move.moveTo(start.x, start.y, start.z)
 storage.drop(tools.maxSlots)
