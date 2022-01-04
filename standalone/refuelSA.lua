64219fe73ad694efb02887b57ba6ebc5419d5cb9
Julian B <Keigun-Spark@outlook.com>
2022-01-04 22:33:46 +0100

    Changed refuel

--- standalone/refuelSA.lua
+++ standalone/refuelSA.lua
@@ -1,41 +1,45 @@
 local tArgs = {...}
 
 function refuel(secs)
-	if type(tonumber(secs)) ~= "number" or secs ~= nil then
-		if tostring(secs) == "replace" then
-			fs.move("refuelSA.lua", "refuel.lua")
-			term.clear()
-			term.setCursorPos(1,1)
-			print("Replaced standard 'refuel' script. Delete 'refuel' to revert it!")
-		end
-	else
+	if secs == replace then
+		fs.move("refuelSA.lua", "refuel.lua")
+		term.clear()
+		term.setCursorPos(1,1)
+		print("Replaced standard 'refuel' script. Delete 'refuel' to revert it!")
+	elseif type(tonumber(secs)) == "number" then
 		if secs == nil then
 			secs = tonumber(120)
-		end
-		print("Max fuel level: "..turtle.getFuelLimit())
-		print("Old fuel level: "..turtle.getFuelLevel())
-		if turtle.getFuelLevel() == turtle.getFuelLimit() then
+			print("Max fuel level: "..turtle.getFuelLimit())
+			print("Old fuel level: "..turtle.getFuelLevel())
+			if turtle.getFuelLevel() == turtle.getFuelLimit() then
+				term.clear()
+				term.setCursorPos(1,1)
+				print("Turtle is fully refuled!")
+			else
+				term.clear()
+				term.setCursorPos(1,1)
+				print("Turtle will now loop refuel for "..secs.." seconds!")
+				for i=1, tonumber(secs) do
+					turtle.refuel()
+					sleep(.5)
+					if turtle.getFuelLevel() == turtle.getFuelLimit() then
+						term.clear()
+						term.setCursorPos(1,1)
+						print("Turtle is now fully refuled!")
+						break
+					end
+				end
+				term.clear()
+				term.setCursorPos(1,1)
+				print("New fuel level: "..turtle.getFuelLevel())
+			end
+		else
 			term.clear()
 			term.setCursorPos(1,1)
-			print("Turtle is fully refuled!")
-		else
-			print("Turtle will now loop refuel for "..secs.." seconds!")
-			for i=1, tonumber(secs) do
-				turtle.refuel()
-				sleep(.5)
-				if turtle.getFuelLevel() == turtle.getFuelLimit() then
-					term.clear()
-					term.setCursorPos(1,1)
-					print("Turtle is now fully refuled!")
-					break
-				end
-			end
-			print("New fuel level: "..turtle.getFuelLevel())
+			error("Please insert a valid time in seconds!")
 		end
 	end
 end
 
-refuel(tArgs[1])
+refuel(tostring(tArgs[1]))
 os.sleep(5)
-term.clear()
-term.setCursorPos(1,1)
