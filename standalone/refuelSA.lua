699447839c5bb2e98c2089313367e81ff7d0e4c2
Julian B <Keigun-Spark@outlook.com>
2022-01-04 22:48:47 +0100

    Changed refuel

--- standalone/refuelSA.lua
+++ standalone/refuelSA.lua
@@ -17,21 +17,15 @@ function refuel(secs, repl)
 		term.setCursorPos(1,1)
 		print("Turtle is fully refuled!")
 	else
-		term.clear()
-		term.setCursorPos(1,1)
 		print("Turtle will now loop refuel for "..secs.." seconds!")
 		for i=1, tonumber(secs) do
 			turtle.refuel()
 			sleep(.5)
 			if turtle.getFuelLevel() == turtle.getFuelLimit() then
-				term.clear()
-				term.setCursorPos(1,1)
 				print("Turtle is now fully refuled!")
 				break
 			end
 		end
-		term.clear()
-		term.setCursorPos(1,1)
 		print("New fuel level: "..turtle.getFuelLevel())
 	end
 end
@@ -45,4 +39,6 @@ if type(tonumber(tArgs[1])) ~= "number" then
 end
 
 refuel(tonumber(tArgs[1]), tostring(tArgs[2]))
+term.clear()
+term.setCursorPos(1,1)
 os.sleep(5)
