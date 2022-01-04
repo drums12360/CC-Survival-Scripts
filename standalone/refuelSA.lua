0824f25c114e6e9d969260c7736f9e22981862c3
Julian B <Keigun-Spark@outlook.com>
2022-01-04 22:47:26 +0100

    Bug fix

--- standalone/refuelSA.lua
+++ standalone/refuelSA.lua
@@ -1,45 +1,48 @@
 local tArgs = {...}
 
-function refuel(secs)
-	if secs == replace then
+function refuel(secs, repl)
+	if repl == replace then
 		fs.move("refuelSA.lua", "refuel.lua")
 		term.clear()
 		term.setCursorPos(1,1)
 		print("Replaced standard 'refuel' script. Delete 'refuel' to revert it!")
-	elseif type(tonumber(secs)) == "number" then
-		if secs == nil then
-			secs = tonumber(120)
-			print("Max fuel level: "..turtle.getFuelLimit())
-			print("Old fuel level: "..turtle.getFuelLevel())
+	end
+	if secs == nil then
+		secs = tonumber(120)
+	end
+	print("Max fuel level: "..turtle.getFuelLimit())
+	print("Old fuel level: "..turtle.getFuelLevel())
+	if turtle.getFuelLevel() == turtle.getFuelLimit() then
+		term.clear()
+		term.setCursorPos(1,1)
+		print("Turtle is fully refuled!")
+	else
+		term.clear()
+		term.setCursorPos(1,1)
+		print("Turtle will now loop refuel for "..secs.." seconds!")
+		for i=1, tonumber(secs) do
+			turtle.refuel()
+			sleep(.5)
 			if turtle.getFuelLevel() == turtle.getFuelLimit() then
 				term.clear()
 				term.setCursorPos(1,1)
-				print("Turtle is fully refuled!")
-			else
-				term.clear()
-				term.setCursorPos(1,1)
-				print("Turtle will now loop refuel for "..secs.." seconds!")
-				for i=1, tonumber(secs) do
-					turtle.refuel()
-					sleep(.5)
-					if turtle.getFuelLevel() == turtle.getFuelLimit() then
-						term.clear()
-						term.setCursorPos(1,1)
-						print("Turtle is now fully refuled!")
-						break
-					end
-				end
-				term.clear()
-				term.setCursorPos(1,1)
-				print("New fuel level: "..turtle.getFuelLevel())
+				print("Turtle is now fully refuled!")
+				break
 			end
-		else
-			term.clear()
-			term.setCursorPos(1,1)
-			error("Please insert a valid time in seconds!")
 		end
+		term.clear()
+		term.setCursorPos(1,1)
+		print("New fuel level: "..turtle.getFuelLevel())
 	end
 end
 
-refuel(tostring(tArgs[1]))
+if type(tonumber(tArgs[1])) ~= "number" then
+	if type(tonumber(tArgs[1])) ~= "nil" then
+		term.clear()
+		term.setCursorPos(1,1)
+		error("Define step amount and direction! (Example: '10 up') [10 steps, upwards]")
+	end
+end
+
+refuel(tonumber(tArgs[1]), tostring(tArgs[2]))
 os.sleep(5)
--- standalone/stripTunnelSA.lua
+++ standalone/stripTunnelSA.lua
@@ -696,7 +696,7 @@ function mineSquence(Shaft_Amount, Shaft_Width, Shaft_Distance)
 	end
 end
 
-if type(tonumber(tArgs[1])) and type(tonumber(tArgs[2])) and type(tonumber(tArgs[3])) ~= "number" then
+if type(tonumber(tArgs[1])) ~= "number" and type(tonumber(tArgs[2])) ~= "number" and type(tonumber(tArgs[3])) ~= "number" then
 	term.clear()
 	term.setCursorPos(1,1)
 	error("Define shaft amount, shaft width and shaft distance! (Example: '10 20 3') [10 deep, 20 to each side, and every 3 blocks]")
